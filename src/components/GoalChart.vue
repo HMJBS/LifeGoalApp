8afc2b2baaa51d02ace9179d80b2502e974b9666
yohei yamamoto <yamarail.pc@live.jp>
2020-01-26 00:02:59 +0900

    added vue.route

---
+++ src/components/GoalChart.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <div id='chart'>
+    <h1>{{ userName }}'s Life Goal</h1>
+    <table>
+      <tr>
+        <th>Ultimate Goal</th>
+        <th>Big Goal</th>
+        <th>Needed Skill</th>
+        <th>Must-do task</th>
+      </tr>
+      <tr>
+        <td>"Be Big One"</td>
+        <td></td>
+        <td></td>
+        <td>This is still test</td>
+      </tr>
+    </table>
+  </div>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'GoalChart',
+    props: {
+      userName: String
+    }
+  }
+
+</script>
+
+<style scoped>
+
+
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="home">
+  <GoalChart />
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GoalChart from '@/components/GoalChart.vue'
+export default {
+  name: 'home',
+  components: {
+    GoalChart
+  }
+}
+</script>
