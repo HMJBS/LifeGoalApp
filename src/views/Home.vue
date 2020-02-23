e374de93f1472dcae7afa2139776890e15459415
unknown <yamarail.pc@live.jp>
2020-02-23 22:05:23 +0900

    add icon to treechart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
   <GoalChart
   userName="Pootis"
   :goalsData="this.rawGoalData" />
+  <a target="_blank" href="https://icons8.com/icons/set/lifecycle">Life Cycle</a>, <a target="_blank" href="https://icons8.com/icons/set/experience-skill">Experience Skill</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a>
+
 </div>
 </template>
 
