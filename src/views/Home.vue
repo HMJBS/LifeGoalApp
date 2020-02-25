ed6074c77c46249991c8ccef602a852a415c52cb
unknown <yamarail.pc@live.jp>
2020-02-25 22:50:54 +0900

    remove local goalData.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
 <div class="home">
   <GoalChart
-  userName="Jeremy"
-  :goalsData="this.rawGoalData" />
+  :userName="userName" />
   <a target="_blank" href="https://icons8.com/icons/set/lifecycle">Life Cycle</a>, <a target="_blank" href="https://icons8.com/icons/set/experience-skill">Experience Skill</a> and other icons by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 </div>
@@ -11,7 +10,7 @@
 <script>
 // @ is an alias to /src
 import GoalChart from '@/components/GoalChart.vue'
-import rawGoalDataJson from '../assets/goalData.json'
+
 export default {
   name: 'home',
   components: {
@@ -19,7 +18,7 @@ export default {
   },
   data(){
     return {
-      rawGoalData: rawGoalDataJson
+      userName: 'jeremy'
     }
   }
 }
