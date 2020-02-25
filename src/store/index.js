969fea22335190adc75596abb986aca9cdf645eb
unknown <yamarail.pc@live.jp>
2020-02-26 00:02:58 +0900

    add vuex.store for handle lifeObjects rename lifeGoal -> lifeObjects

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,53 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
+    /**
+     * user's Name
+     * passed to REST /{username}
+     * @type {String}
+     */
+    userName: null,
+
+    /**
+     * user's objects
+     * formatted for tree templete
+     * @type {Object}
+     */
+    lifeObjects: {}
   },
   mutations: {
+
+    setUserName(state, payload) {
+      state.userName = payload.userName;
+    },
+
+    setLifeObjects(state, payload) {
+      state.lifeObjects = payload.lifeObjects;
+    }
+
   },
   actions: {
+    async getLIfeObjectByCurrentUser({ commit, state }) {
+
+      try {
+
+        const userName = state.userName;
+        if (!userName) { throw `Invalid UserName ${userName}`; }
+        const res = await axios.get(`http://localhost:7005/${state.userName}`);
+        const { lifeObjects } = res;
+        commit('setLifeObjects', { lifeObjects });
+
+      } catch (err) {
+
+        throw err;
+      }
+    }
   },
   modules: {
   }
