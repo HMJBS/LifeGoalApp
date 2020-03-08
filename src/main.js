0b58f284827f6163015014a34000d902de99abd5
unknown <yamarail.pc@live.jp>
2020-03-08 21:59:53 +0900

    add Backend address and host in env file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dotenv from 'dotenv'
+
+//import .env file
+dotenv.config();
 
 Vue.config.productionTip = false
 
