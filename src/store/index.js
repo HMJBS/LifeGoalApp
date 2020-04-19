fd949567aeac0ad7a13976f002a7cb0656c58444
unknown <yamarail.pc@live.jp>
2020-04-19 23:18:43 +0900

    implement life object view

--- src/store/index.js
+++ src/store/index.js
@@ -46,10 +46,13 @@ export default new Vuex.Store({
 
         const userName = state.userName;
         if (!userName) { throw `Invalid UserName ${userName}`; }
-        console.log(`http://${process.env.VUE_APP_BE_HOST}:${process.env.VUE_APP_BE_PORT}/user/${state.userName}`);
+
+        console.log(`GET /user/${state.userName}`);
+
         const res = await ax.get(`/user/${state.userName}`);
         const { data } = res;
-        const objLifeObjects = JSON.parse( data.lifeObjects );
+        const objLifeObjects = buildTreeStruct( data.lifeObjects, userName );
+
         commit('setLifeObjects', { lifeObjects: objLifeObjects });
 
       } catch (err) {
@@ -75,3 +78,116 @@ export default new Vuex.Store({
     }
   }
 });
+
+/**
+ * create tree object used for vue-tree
+ * @param {Array} lifeObjects raw array lifeObjects from GET /user/:userName
+ * @param {String} userName
+ * @returns {Object} tree object readable by tree-vue 
+ */
+function buildTreeStruct(lifeObjects, userName) {
+
+  const tree = {
+    name: `${userName}'s Life Object`,
+    children: [],
+  };
+
+  const depthSorted = [];
+
+  const imageByDepth = [
+    "lifegoal.png",
+    "skill.png",
+    "knowledge.png",
+    "study.png",
+  ];
+
+  // sorts elements by layer depth
+  lifeObjects.forEach((elem) => {
+
+    const depth = elem.layerDepth;
+    if (!depthSorted[depth]) { depthSorted[depth] = []; }
+    depthSorted[depth].push(elem);
+  
+  });
+
+  // push elements to tree from deep element
+  depthSorted.forEach((depth) => {
+
+    depth.forEach((node) => {
+
+      const clonedNode = {};
+      const cloneDepth = node.layerDepth;
+
+      // deep copy to clonedElem
+      Object.assign(clonedNode, node);
+
+      if (cloneDepth === 0) {
+
+        // add image_url
+        clonedNode.image_url = imageByDepth[0];
+
+        // clear children, it must be tree structure
+        clonedNode.children = [];
+        tree.children.push(clonedNode);
+
+      } else {
+
+        // add image_url
+        clonedNode.image_url = imageByDepth[cloneDepth];
+
+        // clear children, it must be tree structure
+        clonedNode.children = [];
+
+        let parentNode = null;
+
+        // search parent node by "_id"
+
+        if (cloneDepth === 1) {
+
+          tree.children.forEach((element1) => {
+
+            if (!parentNode) {
+              parentNode = element1.children.find(element2 => element2.id === clonedNode._id);
+            }
+          });
+
+        } else if (cloneDepth === 2) {
+
+          tree.children.forEach((element1) => {
+            element1.children.forEach((element2) => {
+
+              if (!parentNode) {
+                parentNode = element2.children.find(element3 => element3.id === clonedNode._id);
+              }
+            });
+          });
+
+        } else if (cloneDepth === 3) {
+
+          tree.children.forEach((element1) => {
+            element1.children.forEach((element2) => {
+              element2.children.forEach((element3) => {
+
+                if (!parentNode) {
+                  parentNode = element3.children.find(element4 => element4.id === clonedNode._id);
+                }
+              });
+            });
+          });
+
+        }
+
+        if (!parentNode) {
+
+          console.error(`[buildTreeStruct] Node without parent`);
+          console.error(clonedNode);
+          return;
+
+        } 
+
+        parentNode.children.push(clonedNode);
+      }
+    });
+  });
+  return tree;
+}
