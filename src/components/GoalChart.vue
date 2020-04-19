dec959a269b93d0e37ccbd8a0952403821db30a3
unknown <yamarail.pc@live.jp>
2020-04-19 20:10:06 +0900

    apply vue-bootstrap

--- src/components/GoalChart.vue
+++ src/components/GoalChart.vue
@@ -1,7 +1,17 @@
 <template>
 
   <div id='chart'>
-    <TreeChart v-if='objectsJson' :json='objectsJson' />
+    <TreeChart v-if='objectsJson' :json='objectsJson' @click-node="addObjectModal" />
+    <modal name="addObject" :draggable="true" :resizable="true">
+      <h2>Add new Object</h2>
+      <form>
+        <label for="newObjectString">new object</label>
+        <input id="newObjectString" type="text" v-model="newObjectStr">
+        <input type="submit" value="Register" @click="registerNewObject" 
+            @submit.prevent="registerNewObject" >
+      </form>
+        
+    </modal>
   </div>
 
 </template>
@@ -19,9 +29,36 @@
         return this.$store.state.lifeObjects;
       }
     },
+    data() {
+      return {
+        newObjectStr: ''
+      }
+    },
     name: 'GoalChart',
-    mounted() {
-
+    methods: {
+      addObjectModal: function() {
+        this.$modal.show('addObject');
+      },
+      hideObjectModal: function() {
+        this.$modal.hide('addObject');
+      },
+      registerNewObject: function() {
+        
+        // eslint-disable-next-line no-console
+        console.log(this);
+        // check clikced node has children
+        if (this.children) {
+          
+          this.children.push({
+            extend: true,
+            finished: false,
+            image_url: "study.png",
+            name: this.newObjectStr
+          });
+          this.$store.commit('setLifeObject', {lifeObjects: this.children});
+        }
+        this.$modal.hide('addObject');
+      }
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueJSModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
+Vue.use(VueJSModal);
 
 new Vue({
   router,
