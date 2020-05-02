fdf1f8a8faee44766ce28b8aaec6558081134c39
unknown <yamarail.pc@live.jp>
2020-05-02 14:45:08 +0900

    add userName

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <b-navbar toggleable="lg" type="dark" variant="info">
 
-      <b-navbar-brand to="/">Life Object Tracker</b-navbar-brand>
+      <b-navbar-brand class="font-weight-bolder" to="/">Life Goal Tracker</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
@@ -10,6 +10,11 @@
         <b-navbar-nav>
           <b-nav-item to="/user">User</b-nav-item>
           <b-nav-item to="/about" >About</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right Alligned-->
+        <b-navbar-nav class="ml-auto">
+          <b-avatar variant="primary" rounded>{{ userName }}</b-avatar>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
