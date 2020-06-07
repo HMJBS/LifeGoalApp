1545d20d4039bda27e7a4d1f6d8f7c10f5a15cac
unknown <yamarail.pc@live.jp>
2020-06-07 18:07:21 +0900

    add card template

--- src/components/GoalChart.vue
+++ src/components/GoalChart.vue
@@ -1,75 +1,82 @@
 <template>
+  <div class="mb-4">
+    <b-card
+      title="Be Good Programmer"
+      sub-title="Be skillful programmer enough to work as professional."
+      img-src="https://picsum.photos/600/300/?image=25"
+      img-alt="Image"
+      img-top
+      tag="Goal"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>Needed Skills</b-card-text>
+      <b-card title="Business Analysitics skills" sub-title="REBOK I guess.">
+        <b-icon-arrow-down-short v-b-toggle.collapse-1 class="h3" />
+        <b-collapse id="collapse-1" class="mb-2">
+          <b-card title="Learn Group Facilitation Skill" sub-title="Goup">
+            <b-icon-arrow-down-short v-b-toggle.collapse-1-1 class="h3" />
+            <b-collapse id="collapse-1-1" class="mb-2">
+              <b-card
+                title="Read Business Analystic Vol.2"
+                sub-title="Read'em All"
+              >
+              </b-card>
+              <b-card
+                title="Read Business Analystic Vol.2"
+                sub-title="Read'em All"
+              >
+              </b-card>
+              
+            </b-collapse>
+          </b-card>
+        </b-collapse>
+      </b-card>
 
-  <div id='chart'>
-    <TreeChart v-if='objectsJson' :json='objectsJson.data' @click-node="showAddObjectModal" />
-    <b-modal id="addOjectModal" title="Add new object" @ok="registerNewObject">
-      <b-form>
+      <b-card title="Soft skilz, mate" sub-title="REBOK I guess."></b-card>
 
-        <b-form-group
-          id="input-group-new-object"
-          label="New Life Object"
-          label-for="input-new-object-string"
-        >
-          <b-form-input
-            id="input-new-object-string"
-            v-model="newObjectStr"
-            type="text"
-            required
-            placeholder="New Object"
-          ></b-form-input>
-        </b-form-group>
-
-      </b-form>
-    </b-modal>
+      <b-card title="Business Analysitics skills" sub-title="REBOK I guess."></b-card>
+    </b-card>
   </div>
-
 </template>
 
 <script>
-
-  import TreeChart from 'vue-tree-chart'
-
-  export default {
-    components: {
-      TreeChart
+export default {
+  computed: {
+    objectsJson() {
+      return this.$store.state.lifeObjects;
+    }
+  },
+  data() {
+    return {
+      newObjectStr: "",
+      clickedNode: null
+    };
+  },
+  name: "GoalChart",
+  methods: {
+    showAddObjectModal: function(payload) {
+      this.clickedNode = payload;
+      this.$bvModal.show("addOjectModal");
     },
-    computed: {
-      objectsJson () {
-        return this.$store.state.lifeObjects;
-      }
+    hideObjectModal: function() {
+      this.$bvModal.hide("addOjectModal");
     },
-    data() {
-      return {
-        newObjectStr: '',
-        clickedNode: null
-      }
-    },
-    name: 'GoalChart',
-    methods: {
-      showAddObjectModal: function(payload) {
+    registerNewObject: function() {
+      // check clikced node has children
+      this.$store.dispatch("registerNewObject", {
+        name: this.newObjectStr,
+        finished: false,
+        parentId: this.clickedNode._id
+      });
 
-        this.clickedNode = payload;
-        this.$bvModal.show('addOjectModal');
-      },
-      hideObjectModal: function() {
-        this.$bvModal.hide('addOjectModal');
-      },
-      registerNewObject: function() {
-        
-        // check clikced node has children
-        this.$store.dispatch('registerNewObject', 
-          { name: this.newObjectStr, finished: false, parentId: this.clickedNode._id });
-
-        this.$modal.hide('addObject');
-      }
+      this.$modal.hide("addObject");
     }
   }
-
-
+};
 </script>
 
 <style scoped>
-
 table {
   width: 100%;
   border: 2px solid Black;
@@ -77,7 +84,7 @@ table {
   table-layout: fixed;
 }
 
-thead th{
+thead th {
   padding: 10px 5px;
   font: 1.5em sans-serif, bold;
 }
@@ -94,5 +101,4 @@ tbody tr {
   padding: 5px 3px;
   font: 1.2em serif;
 }
-
 </style>
