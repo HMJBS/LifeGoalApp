3b9f3f7ebaa3d60e56167713506bcff4d339203e
unknown <yamarail.pc@live.jp>
2020-06-07 22:50:57 +0900

    implement expandable card GUI

--- src/components/GoalChart.vue
+++ src/components/GoalChart.vue
@@ -1,74 +1,42 @@
 <template>
   <div class="mb-4">
-    <b-card-group deck>
-    <b-card
-      title="Be Good Programmer"
-      sub-title="Be skillful programmer enough to work as professional."
-      tag="Goal"
+    I'm here.
+    <b-card-group deck  v-if="objectsJson">
+
+    <!-- Life Object layer -->
+    <b-card v-for="lifeGoal in objectsJson.data.children" v-bind:key="lifeGoal._id"
+      v-bind:title="lifeGoal.name"
+      sub-title="None"
       class="mb-2"
     >
       <b-card-text>Needed Skills</b-card-text>
-      <b-card title="Business Analysitics skills" sub-title="REBOK I guess.">
-        <b-icon-arrow-down-short v-b-toggle.collapse-1 class="h3" />
-        <b-collapse id="collapse-1" class="mb-2">
-          <b-card title="Learn Group Facilitation Skill" sub-title="Goup">
-            <b-icon-arrow-down-short v-b-toggle.collapse-1-1 class="h3" />
-            <b-collapse id="collapse-1-1" class="mb-2">
-              <b-card
-                title="Read Business Analystic Vol.2"
+      <!-- skillset layer -->
+      <b-card v-for="skillset in lifeGoal.children" :key="skillset._id"
+        :title="skillset.name"
+        sub-title="REBOK I guess."
+      >
+        <!-- v-binding v-b-toggle don"t work -->
+        <b-icon-arrow-down-short v-b-toggle="`collapse-${skillset._id}`" class="h3" />
+        <b-collapse class="mb-2" :id="`collapse-${skillset._id}`">
+          <!-- skill layer -->
+          <b-card v-for="skill in skillset.children" :key="skill._id" 
+            :title="skill.name"
+            sub-title="Goup"
+          >
+            <b-icon-arrow-down-short v-b-toggle="'collapse-' + skill._id"  class="h3" />
+            <b-collapse :id="'collapse-' + skill._id " class="mb-2">
+              <!-- study layer -->
+              <b-card v-for="study in skill.children" :key="study._id"
+                :title="study.name"
                 sub-title="Read'em All"
               >
               </b-card>
-              <b-card
-                title="Read Business Analystic Vol.2"
-                sub-title="Read'em All"
-              >
-              </b-card>
-              
             </b-collapse>
           </b-card>
         </b-collapse>
       </b-card>
-
-      <b-card title="Soft skilz, mate" sub-title="REBOK I guess."></b-card>
-
-      <b-card title="Business Analysitics skills" sub-title="REBOK I guess."></b-card>
-    </b-card>
-        <b-card
-      title="Be Good Programmer"
-      sub-title="Be skillful programmer enough to work as professional."
-      tag="Goal"
-      class="mb-2"
-    >
-      <b-card-text>Needed Skills</b-card-text>
-      <b-card title="Business Analysitics skills" sub-title="REBOK I guess.">
-        <b-icon-arrow-down-short v-b-toggle.collapse-1 class="h3" />
-        <b-collapse id="collapse-1" class="mb-2">
-          <b-card title="Learn Group Facilitation Skill" sub-title="Goup">
-            <b-icon-arrow-down-short v-b-toggle.collapse-1-1 class="h3" />
-            <b-collapse id="collapse-1-1" class="mb-2">
-              <b-card
-                title="Read Business Analystic Vol.2"
-                sub-title="Read'em All"
-              >
-              </b-card>
-              <b-card
-                title="Read Business Analystic Vol.2"
-                sub-title="Read'em All"
-              >
-              </b-card>
-              
-            </b-collapse>
-          </b-card>
-        </b-collapse>
-      </b-card>
-
-      <b-card title="Soft skilz, mate" sub-title="REBOK I guess."></b-card>
-
-      <b-card title="Business Analysitics skills" sub-title="REBOK I guess."></b-card>
     </b-card>
     </b-card-group>
-    
   </div>
 </template>
 
@@ -103,34 +71,14 @@ export default {
       });
 
       this.$modal.hide("addObject");
+    },
+    generateCollapseId: function( collapseId ) {
+      return 'collapse-' + collapseId;
     }
   }
 };
 </script>
 
 <style scoped>
-table {
-  width: 100%;
-  border: 2px solid Black;
-  border-collapse: collapse;
-  table-layout: fixed;
-}
 
-thead th {
-  padding: 10px 5px;
-  font: 1.5em sans-serif, bold;
-}
-
-tbody tr:nth-child(odd) {
-  background-color: LightGrey;
-}
-
-tbody tr:nth-child(even) {
-  background-color: White;
-}
-
-tbody tr {
-  padding: 5px 3px;
-  font: 1.2em serif;
-}
 </style>
